9b921ef036746ed89de6e14a77bb58d40252e425
dyldlewis <dyldlewis@gmail.com>
2021-06-11 13:00:46 -0700

    add font and change hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,3 +1,9 @@
+import "./styles.css";
 export default function Hero() {
-  return <h1>Hi, I'm Dylan Lewis. I'm a full-stack web developer.</h1>;
+  return (
+    <h1>
+      Hi, I'm <span className="fullName">Dylan Lewis</span>. I'm a full-stack
+      web developer.
+    </h1>
+  );
 }
