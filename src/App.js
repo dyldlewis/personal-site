befa88366657f698ea351892374bf03435f753ee
dyldlewis <dyldlewis@gmail.com>
2021-06-16 14:35:28 -0700

    add header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from "./logo.svg";
 import ProfilePicture from "./components/ProfilePicture";
 import Hero from "./components/Hero";
+import WorkTiles from "./components/WorkTiles";
+import WorkLinks from "./components/WorkLinks";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
+      <Header />
       <ProfilePicture />
       <Hero />
+      <WorkTiles />
+      <WorkLinks />
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+export default function Header() {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  );
+}
