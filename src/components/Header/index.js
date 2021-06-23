75c58dd833c57efc00e21342dfd05937f8fe8ba1
dyldlewis <dyldlewis@gmail.com>
2021-06-22 22:03:33 -0700

    clean up nav and skill tiles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 export default function Header() {
   return (
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
+    <div className="header">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
   );
 }
--- src/components/SkillTiles/index.js
+++ src/components/SkillTiles/index.js
@@ -4,5 +4,10 @@ const skills = ["React", "CSS", "Bash Scripting", "Postgres", "Node"];
 const skillsList = skills.map((skill) => <div>{skill}</div>);
 
 export default function SkillTiles() {
-  return <div className="skills">{skillsList}</div>;
+  return (
+    <div className="pageContainer">
+      <h1>Skills</h1>
+      <div className="skills">{skillsList}</div>
+    </div>
+  );
 }
